7920fe9a5ff26fa8bd438efc374d6dd3a2c1834b
szscnc <88357194+szscnc@users.noreply.github.com>
2024-06-22 15:09:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.szsc.us.kg": dockerHub,
+  "quay.szsc.us.kg": "https://quay.io",
+  "gcr.szsc.us.kg": "https://gcr.io",
+  "k8s-gcr.szsc.us.kg": "https://k8s.gcr.io",
+  "k8s.szsc.us.kg": "https://registry.k8s.io",
+  "ghcr.szsc.us.kg": "https://ghcr.io",
+  "cloudsmith.szsc.us.kg": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.szsc.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
